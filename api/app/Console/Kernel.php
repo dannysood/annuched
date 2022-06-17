1df828dce169c29bbd62eed162b3b8731153328b
Pulkit Sood <soodpulkit21@gmail.com>
2022-06-17 16:53:40 +0530

    feat(blog): :sparkles: check for new posts every 15 minutes

--- api/app/Console/Kernel.php
+++ api/app/Console/Kernel.php
@@ -15,7 +15,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('remote-posts:load')->everyMinute()->onOneServer();
+        $schedule->command('remote-posts:load')->everyFifteenMinutes()->onOneServer();
         // $schedule->command('inspire')->hourly();
     }
 
