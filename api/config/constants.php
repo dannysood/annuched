588bca624f849a5c8148e761e1f1511f89d1d668
Pulkit Sood <soodpulkit21@gmail.com>
2022-06-22 15:33:48 +0530

    test(test): :white_check_mark: bumping rate limit for testing to 60000 temporarily

--- api/config/constants.php
+++ api/config/constants.php
@@ -1,7 +1,7 @@
 <?php
 return [
     'pageSize' => 4,
-    'rateLimit' => 60,
+    'rateLimit' => 60000,
     'cache' => [
         'posts' => [
             'paginationPrefixRoot' => 'posts-pagination-first',
