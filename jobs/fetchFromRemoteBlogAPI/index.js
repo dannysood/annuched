a8e5af9d12cf4e6e6c1c3a6a3a85829e9f6a7b2d
Pulkit Sood <soodpulkit21@gmail.com>
2022-06-22 16:12:00 +0530

    fix(ci): :bug: move to post raw json body for job

--- jobs/fetchFromRemoteBlogAPI/index.js
+++ jobs/fetchFromRemoteBlogAPI/index.js
@@ -6,7 +6,11 @@
  */
 const axios = require('axios').default;
 exports.fetchFromRemoteBlogAPI = (event, context) => {
-  return axios.post(`https://api.annuched.dannysood.com/api/v1/job/fetch-from-remote-blog-api?key=${process.env.JOBS_PRESHARED_SECRET}`)
+  return axios.post(`https://api.annuched.dannysood.com/api/v1/job/fetch-from-remote-blog-api`,{"key":$process.env.JOBS_PRESHARED_SECRET},{headers: {
+    "Accept": "application/json",
+    "Content-Type": "application/json",
+
+}})
     .then((response) => console.log("*******SUCCESS:", response))
     .catch((error) => console.log("!!!!!!!FAILURE:", error))
 };
